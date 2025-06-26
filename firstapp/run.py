4969403882dd1102dc6b87a396707e47fc7e76f2
Rajat <rajatsharma82004@gmail.com>
2025-06-26 23:25:08 +0530

    change in app context

--- firstapp/run.py
+++ firstapp/run.py
@@ -4,5 +4,7 @@ from models import Donor
 flask_app = create_app()
 
 if __name__ == '__main__':
-      db.create_all()
+      with flask_app.app_context():
+            db.create_all()
+            
       flask_app.run(host='0.0.0.0',debug=True) 
